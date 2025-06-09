fc3872c238671e236636ae1cd0be4ae873eb70f0
Felipe <lipe1090@icloud.com>
2025-06-09 18:10:33 -0300

    style: novo layout home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,45 +1,52 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HeaderComponent } from '../../template/header/header.component';
-import { FooterComponent } from '../../template/footer/footer.component';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Leagues } from '../../models/leagues.model';
 import { Router, RouterLink } from '@angular/router';
 import { ToasterService } from '../../services/toaster.service';
+import { LeaguesService } from '../../services/leagues.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterLink],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  leagues: Leagues[] = [
-    {
-      name: 'Liga Verssat',
-      img: 'verssat.jpeg',
-      private: false,
-      organizer: [
-        {
-          name: 'Felipe',
-          phone: '119398349',
-          email: 'felipe@icloud.com'
-        }
-      ]
-    },
-    { name: 'Copa do Brasil', img: '', private: false, organizer: [{ name: 'JJ' }] },
-    { name: 'Brasileirão', img: '', private: false, organizer: [{ name: 'JJ' }] },
-  ];
 
   private router = inject(Router);
   private toaster = inject(ToasterService);
-  
-  ngOnInit() {
+  private leaguesService = inject(LeaguesService);
+
+  filtro = signal('');
+  leagues = signal<Leagues[]>([]);
+  isLoading = signal(true);
+
+  async ngOnInit() {
     const msg = localStorage.getItem('mensagem_ativacao');
     if (msg) {
       this.toaster.success(msg);
       localStorage.removeItem('mensagem_ativacao');
     }
 
+    this.isLoading.set(true);
+    try {
+      const all = await this.leaguesService.getAllLeagues();
+      this.leagues.set(all);
+    } catch (err) {
+      this.toaster.error('Erro ao carregar ligas');
+      console.error(err);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
+
+  ligasFiltradas(): Leagues[] {
+    const termo = this.filtro().toLowerCase().trim();
+    return this.leagues().filter(liga =>
+      liga.name.toLowerCase().includes(termo)
+    );
   }
 
   imagemPadrao = 'liga_padrao.png';
